137ecf8f10b0f118f5ab2ffb4a8b7ea59aada9de
trongdatvu <trongdat@unstatic.co>
2023-11-30 16:41:14 +0700

    chore: update lib

--- lib/components/Map.d.ts
+++ lib/components/Map.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="apple-mapkit-js-browser" />
-import React from 'react';
-import MapProps from './MapProps';
+import React from "react";
+import MapProps from "./MapProps";
 declare const Map: React.ForwardRefExoticComponent<MapProps & {
     children?: React.ReactNode;
 } & React.RefAttributes<mapkit.Map | null>>;
